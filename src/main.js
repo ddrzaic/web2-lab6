d4d35419eb7c04979cd830944a742aa6f8392e80
ddrzaic <dino.drzaic.zg.14@gmail.com>
2023-01-16 16:08:40 +0100

    remove About

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import About from "./About.vue";
 import router from "./router";
 import { createPinia } from "pinia";
 
@@ -8,8 +7,3 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount("#app");
-
-const about = createApp(About);
-about.use(createPinia());
-about.use(router);
-about.mount("#about");
