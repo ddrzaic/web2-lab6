bcbea42f4ac0e6eedf1ce14e7608da819cb5cf99
ddrzaic <dino.drzaic.zg.14@gmail.com>
2023-01-16 13:29:45 +0100

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ShopView from "../views/ShopView.vue";
 import AboutView from "../views/AboutView.vue";
+import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: AboutView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: NotFound,
     },
   ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper">
+    <h1>404</h1>
+    <p>Page not found</p>
+    <router-link to="/">Go to home page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
