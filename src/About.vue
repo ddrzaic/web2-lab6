cc9867bb15b516806e49765755fe34d0f5c97fb9
ddrzaic <dino.drzaic.zg.14@gmail.com>
2023-01-16 16:06:13 +0100

    add another page

---
+++ src/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="wrapper">
+      <router-link class="home" to="/">Return to home page</router-link>
+      <h1>About this page</h1>
+      <p>
+        This is a simple page where you can see the list of Ali kebaba shops in
+        Croatia and all their details.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+};
+</script>
+<style scoped>
+h1 {
+  margin: 70px 0px 20px;
+  font-size: 30px;
+}
+</style>
